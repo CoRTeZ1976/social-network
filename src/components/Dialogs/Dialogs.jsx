a4ca9baf76ba201c12c3e84c6b1914e9c12f1aac
Aleksey Shchendrygin <67939931+CoRTeZ1976@users.noreply.github.com>
2021-11-21 17:31:56 +0300

    Edit dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,20 +5,33 @@ import Messages from "./Dialog/Messages/Messages";
 
 
 const Dialogs = (props) => {
+
+	let dialogsData = [
+		{id: 1, name: '1'},
+		{id: 2, name: '2'},
+		{id: 3, name: '3'},
+		{id: 4, name: '4'},
+		{id: 5, name: '5'},
+		{id: 6, name: '6'},
+	];
+
+	let messagesData = [
+		{id: 1, message: 'Hi'},
+		{id: 2, message: 'How are you?'},
+		{id: 3, message: 'I am fine'},
+	];
+
 	return (
 		<div className={dialogsClasses.dialogs}>
 			<div className={dialogsClasses.dialogItems}>
-				<DialogItem name='Dialog 1' id='1'/>
-				<DialogItem name='Dialog 2' id='2'/>
-				<DialogItem name='Dialog 3' id='3'/>
-				<DialogItem name='Dialog 4' id='4'/>
-				<DialogItem name='Dialog 5' id='5'/>
-				<DialogItem name='Dialog 6' id='6'/>
+				<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+				<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+
 			</div>
 			<div className={dialogsClasses.messages}>
-				<Messages message={"Hi"}/>
-				<Messages message={"How are you?"}/>
-				<Messages message={"I am fine"}/>
+				<Messages message={messagesData[0].message}/>
+				<Messages message={messagesData[1].message}/>
+				<Messages message={messagesData[2].message}/>
 			</div>
 		</div>
 	);
--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import Post from "./Posts/Post";
 
 const MyPosts = (props) => {
+
+	let postsData = [
+		{id: 1, message: 'Hi', likes: 3},
+		{id: 2, message: 'How are you?', likes: 5},
+		{id: 3, message: 'I am fine', likes: 7},
+	];
+
 	return (
 		<div>
 			<div>
@@ -10,9 +17,9 @@ const MyPosts = (props) => {
 			<div>
 				<button>Add post</button>
 			</div>
-			<Post message='Hey! How are you?' like='3'/>
-			<Post message='Hey! I am fine. And you?' like='5'/>
-			<Post message='I am fine' like='1'/>
+			<Post message={postsData[0].message} likes={postsData[0].likes}/>
+			<Post message={postsData[1].message} likes={postsData[1].likes}/>
+			<Post message={postsData[2].message} likes={postsData[2].likes}/>
 		</div>
 	);
 }
