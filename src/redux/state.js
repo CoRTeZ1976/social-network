976a805569f4316506f5119105d6bdafe5bf9ef8
CoRTeZ1976 <specmashalexeysh@gmail.com>
2021-11-22 14:28:06 +0300

    add state

---
+++ src/redux/state.js
@@ -0,0 +1,27 @@
+let state = {
+
+	profilePage: {
+		postsData: [
+			{id: 1, message: 'Hi', likes: 3},
+			{id: 2, message: 'How are you?', likes: 5},
+			{id: 3, message: 'I am fine', likes: 7},
+		],
+	},
+
+	dialogsPage: {
+		dialogsData : [
+			{id: 1, name: '1'},
+			{id: 2, name: '2'},
+			{id: 3, name: '3'},
+			{id: 4, name: '4'},
+			{id: 5, name: '5'},
+			{id: 6, name: '6'},
+		],
+		messagesData: [
+			{id: 1, message: 'Hi'},
+			{id: 2, message: 'How are you?'},
+			{id: 3, message: 'I am fine'},
+		],
+	},
+
+}
