2f11a0c644fee3d69206a1a3c4e10a9a6f775500
Aleksey Shchendrygin <67939931+CoRTeZ1976@users.noreply.github.com>
2021-11-30 21:26:52 +0300

    refactoring. add axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,16 @@
 import React from "react";
 import usersStyle from './Users.module.css';
+import *as axios from "axios";
+import userPhoto from "../../asserts/images/images.png"
 
 
 let Users = (props) => {
 
 	if (props.users.length === 0) {
-		props.setUsers([
-				{
-					id: 1,
-					photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC6iPDSqcgCcAtdEz_rPY0B-sxqMd7hz0Hlg&usqp=CAU',
-					followed: true,
-					fullName: 'Jhon',
-					status: 'Hey-hey',
-					location: {city: 'New York', country: 'USA'}
-				},
-				{
-					id: 2,
-					photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC6iPDSqcgCcAtdEz_rPY0B-sxqMd7hz0Hlg&usqp=CAU',
-					followed: false,
-					fullName: 'Alex',
-					status: 'Newer give up!',
-					location: {city: 'Washington', country: 'USA'}
-				},
-				{
-					id: 3,
-					photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC6iPDSqcgCcAtdEz_rPY0B-sxqMd7hz0Hlg&usqp=CAU',
-					followed: true,
-					fullName: 'Anna',
-					status: 'Yo',
-					location: {city: 'Moscow', country: 'Russia'}
-				},
-				{
-					id: 4,
-					photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC6iPDSqcgCcAtdEz_rPY0B-sxqMd7hz0Hlg&usqp=CAU',
-					followed: false,
-					fullName: 'Nikolai',
-					status: 'Привет',
-					location: {city: 'Kiev', country: 'Ukraine'}
-				},
-			],
-		)
+
+		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+			props.setUsers(response.data.items)
+		});
 	}
 
 	return (
@@ -48,22 +19,22 @@ let Users = (props) => {
 				<div className={usersStyle.usersPage}>
 					<div className={usersStyle.photoContainer}>
 						<div>
-							<img alt={'avatar'} src={u.photoUrl} className={usersStyle.usersPhoto}/>
+							<img alt={'avatar'} src={u.photos.small != null ? u.photos.small : userPhoto} className={usersStyle.usersPhoto}/>
 						</div>
 						<div className={usersStyle.toggleFollowBtn}>
 							{u.followed
-								? <button onClick={() => {props.follow(u.id)}}>Follow</button>
-								: <button onClick={() => {props.unfollow(u.id)}}>unfollow</button>}
+								? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+								: <button onClick={() => {props.follow(u.id)}}>Follow</button>}
 						</div>
 					</div>
 					<div>
 						<div className={usersStyle.userName}>
-							<div>{u.fullName}</div>
+							<div>{u.name}</div>
 							<div>{u.status}</div>
 						</div>
-						<div className={usersStyle.locationInfo}>
-							<span>{u.location.city}, {u.location.country}</span>
-						</div>
+						{/*<div className={usersStyle.locationInfo}>
+							<span>{"u.location.city u.location.country"}</span>
+						</div>*/}
 					</div>
 				</div>
 			</div>)
