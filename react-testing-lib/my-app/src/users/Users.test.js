1bbbcd6528da3f60ee7f72165813e0ef0f754dd5
Alex <specmashalexeysh@gmail.com>
2022-04-12 21:09:14 +0300

    add project file

---
+++ react-testing-lib/my-app/src/users/Users.test.js
@@ -0,0 +1,34 @@
+import Users from "./Users";
+import { render } from "react-dom";
+import axios from 'axios';
+
+jest.mock( 'axios' );
+
+describe( "USERS TEST", () => {
+	let response;
+	beforeEach( () => {
+		response = {
+			data: [
+				{
+					"id": 1,
+					"name": "Leanne Graham",
+				},
+				{
+					"id": 2,
+					"name": "Ervin Howell",
+				},
+				{
+					"id": 3,
+					"name": "Clementine Bauch",
+				}],
+		};
+	} );
+	test( 'render learn react link', async () => {
+		axios.get.mockReturnValue(response);
+		render( <Users /> );
+		const users = await screen.findAllByTestId('user-item');
+		expect(users.length).toBe(3);
+		expect(axios.get).toBeCalledTimes(1);
+		screen.debug();
+	} );
+} );
